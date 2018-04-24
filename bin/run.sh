1c2089d918a2c7a7c51826aa4e940066ed5b0d83
Rodrigo Santos <rodrigo@improving.com.br>
2018-04-23 22:58:56 -0300

    Ajuste de pastas

--- bin/run.sh
+++ bin/run.sh
@@ -5,6 +5,8 @@ set -o pipefail
 
 HOSTNAME=${CONTAINER_NAME}
 RESTART="unless-stopped"
+FASTABC_FOLDER="/opt/app/do_run"
+
 
 docker stop ${CONTAINER_NAME} || true
 
@@ -12,4 +14,4 @@ docker rm ${CONTAINER_NAME} || true
 
 docker network create --driver bridge web || true
 
-docker run -d -p8081:8081 --network=web --name=${CONTAINER_NAME} --hostname ${HOSTNAME} --restart ${RESTART} -e JAVA_TIMEZONE=America/Sao_Paulo ${CONTAINER_NAME}
+docker run -d -p8081:8081 --network=web -v "${FASTABC_FOLDER}:${FASTABC_FOLDER}" --name=${CONTAINER_NAME} --hostname ${HOSTNAME} --restart ${RESTART} -e JAVA_TIMEZONE=America/Sao_Paulo ${CONTAINER_NAME}
