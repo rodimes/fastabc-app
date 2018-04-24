889b39464e1a484d4fc77d758664a7a767596df4
Rodrigo Santos <rodrigo@improving.com.br>
2018-04-24 17:18:04 -0300

    Ajustes para utilizar docker image rodimes/spring-boot

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
-FROM improving/java:0.1
+FROM rodimes/spring-boot
 
 MAINTAINER Rodrigo <rodimes@gmail.com>
-WORKDIR /usr/local/bin/
 
 ENV JAVA_TIMEZONE=America/Sao_Paulo
 ENV JAVA_OPTS="-XX:-OmitStackTraceInFastThrow -Xmx512m"
-ENV APP_DIR=/opt/app/do_run
-ENV TARGET_DIR=target
-
-COPY ./bin/fastabc.sh /usr/local/bin/
 
 RUN touch /tmp/test.mv.db
 
 EXPOSE 8081
-CMD ["fastabc.sh"]
--- bin/run.sh
+++ bin/run.sh
@@ -14,4 +14,4 @@ docker rm ${CONTAINER_NAME} || true
 
 docker network create --driver bridge web || true
 
-docker run -d -p8081:8081 --network=web -v "${FASTABC_FOLDER}:${FASTABC_FOLDER}" --name=${CONTAINER_NAME} --hostname ${HOSTNAME} --restart ${RESTART} -e JAVA_TIMEZONE=America/Sao_Paulo ${CONTAINER_NAME}
+docker run -d -p8081:8080 --network=web -v "${FASTABC_FOLDER}:${FASTABC_FOLDER}" --name=${CONTAINER_NAME} --hostname ${HOSTNAME} --restart ${RESTART} -e JAVA_TIMEZONE=America/Sao_Paulo ${CONTAINER_NAME}
